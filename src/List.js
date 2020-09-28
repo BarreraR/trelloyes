f48a59dfde659eb4edf315ddb34bcb97b7ff1ebb
Raul Barrera <rbarrera1992@gmail.com>
2020-09-28 15:36:27 -0500

    added stuff

---
+++ src/List.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './Card.js';
+
+function List(props) {
+    const cards = props.cards.map(card => <Card title={card.title} content={card.content}/> );
+    return (
+        <section className="List">
+          <header className="List-header">
+            <h2>{props.header}</h2>
+          </header>
+          <div className="List-cards">
+            {cards.join("")}
+          </div>
+        </section>
+    );
+}
+
+export default List;
